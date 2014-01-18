44205459421307853f242127a9c59306903f3d59
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-01-17 18:38:57 -0800

    muh

--- index.js
+++ index.js
@@ -7,41 +7,56 @@ var ndarray = require('ndarray')
 var rules = require('./lib/rules.js')
 require('./lib/reqFrame')()
 var getCSS = require('./lib/getCSS')
+var drawGrid = require('./lib/grid.js')
+var w,h,draw,lifeSize,zom,data,data2;
 
-//var time = Time()
-var w = window.innerWidth
-var h = window.innerHeight
-var draw = ui.board.getContext('2d')
-var lifeSize = 25 
-var zoom = 1;
-ui.board.width = w
-ui.board.height = h
+init()
 
-draw.fillStyle = rgba(0,0,0,1) 
-draw.fillRect(0,0,w,h)
-var data = new Uint8ClampedArray(Math.ceil(w / lifeSize) * Math.ceil(h / lifeSize))
-var data2 = new Uint8ClampedArray(Math.ceil(w / lifeSize) * Math.ceil(h / lifeSize))
+function init(){
+  w = window.innerWidth
+  h = window.innerHeight
+  draw = ui.board.getContext('2d')
+  lifeSize = 25 
+  zoom = 1;
+  ui.board.width = w
+  ui.board.height = h
+  drawGrid(draw, w, h, 25)
+}
+
+
+data = new Uint8ClampedArray(Math.ceil(w / lifeSize) * Math.ceil(h / lifeSize))
+data2 = new Uint8ClampedArray(Math.ceil(w / lifeSize) * Math.ceil(h / lifeSize))
 
 for(var x = 0; x < data.length; x++){
   data[x] = 0;
   data2[x] = 0
 }
-var bits = ndarray(data, [Math.ceil(w / lifeSize), Math.ceil(h / lifeSize)]);
+
+var prev = ndarray(data, [Math.ceil(w / lifeSize), Math.ceil(h / lifeSize)]);
 var next = ndarray(data2, [Math.ceil(w / lifeSize), Math.ceil(h / lifeSize)]);
 
-var pixel = draw.getImageData(0,0,100,100)
-//window.requestAnimationFrame(draw)
-function draw(t){
+window.addEventListener('resize', function(evt){
+  init()
+  for(var i = 0; i < next.shape[0]; i++){
+    for(var j = 0; j < next.shape[1]; j++){
+      var n = prev.get(i, j)
+      gen(i * lifeSize, j * lifeSize, n - 10)
+    }
+  }
+}, false)
 
-  window.requestAnimationFrame(draw)
+  var pixel = draw.getImageData(0,0,100,100)
+  function draw(t){
+
+    window.requestAnimationFrame(draw)
   
-}
+  }
 
-var anim = 0;
-touchdown.start(ui.board);
-touchdown.start(ui.step)
-touchdown.start(ui.play)
-touchdown.start(ui.stop)
+  var anim = 0;
+  touchdown.start(ui.board);
+  touchdown.start(ui.step)
+  touchdown.start(ui.play)
+  touchdown.start(ui.stop)
 
 ui.stop.addEventListener('touchdown', function(){
   window.cancelAnimationFrame(anim)
@@ -50,6 +65,12 @@ ui.play.addEventListener('touchdown', function(){
   play()
 })
 ui.step.addEventListener('click', run)
+
+module.exports = {
+  play: play,
+  stop: stop,
+  step: step
+}
 
 function stop(){
   window.cancelAnimationFrame(anim)
@@ -60,31 +81,18 @@ function play(){
 }
 function run(evt){
   
-  rules(bits, next)
+  rules(prev, next)
   for(var i = 0; i < next.shape[0]; i++){
     for(var j = 0; j < next.shape[1]; j++){
       var n = next.get(i, j)
       gen(i * lifeSize, j * lifeSize, n - 10)
     }
   }
-  var z = bits
-  bits = next
+  var z = prev
+  prev = next
   next = z
 
 }
-
-draw.strokeStyle = '#fff';
-for(var x = 0; x < w; x+=lifeSize * zoom){
-  draw.moveTo(x, 0)
-  draw.lineTo(x, h)
-}
-
-for(var y = 0; y < h; y+= lifeSize * zoom){
-  draw.moveTo(0, y)
-  draw.lineTo(w, y)
-}
-draw.stroke()
-
 ui.board.addEventListener('touchdown', springLife)
 //ui.board.addEventListener('deltavector', springLife)
 function gen(x, y, z){
@@ -101,10 +109,10 @@ function springLife(e){
   y -= y % lifeSize
   x /= lifeSize
   y /= lifeSize
-  var z = bits.get(x, y)
+  var z = prev.get(x, y)
   if(z < 10) z = 10
   else z = 0
-  bits.set(x, y, z)
+  prev.set(x, y, z)
   gen(e.detail.x, e.detail.y, z)
 }
 
